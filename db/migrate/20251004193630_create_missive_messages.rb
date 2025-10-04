d5f818e7030f1a18028a5293357a2892878851e0
Hans Lemuet <hans@etamin.studio>
2025-10-04 21:51:10 +0200

    Standard

--- db/migrate/20251004193630_create_missive_messages.rb
+++ db/migrate/20251004193630_create_missive_messages.rb
@@ -3,7 +3,7 @@ class CreateMissiveMessages < ActiveRecord::Migration[8.0]
     create_table :missive_messages do |t|
       t.string :subject, null: false
       t.integer :dispatches_count, default: 0
-      t.references :list, null: false, foreign_key: { to_table: "missive_lists" }
+      t.references :list, null: false, foreign_key: {to_table: "missive_lists"}
       t.string :postmark_message_stream_id
       t.timestamp :sent_at
 
