578d7ce836f6f1f6b83440372ed5164f6a612be4
Hans Lemuet <hans@etamin.studio>
2025-10-04 21:59:59 +0200

    Fix counter_cache

--- app/models/missive/list.rb
+++ app/models/missive/list.rb
@@ -1,6 +1,6 @@
 module Missive
   class List < ApplicationRecord
-    has_many :messages, dependent: :destroy, counter_cache: :messages_count
+    has_many :messages, dependent: :destroy
     # has_many :subscriptions, dependent: :destroy, counter_cache: true
     # has_many :subscriber, through: :subscriptions
 
--- app/models/missive/message.rb
+++ app/models/missive/message.rb
@@ -1,6 +1,6 @@
 module Missive
   class Message < ApplicationRecord
-    belongs_to :list
+    belongs_to :list, counter_cache: :messages_count
 
     time_for_a_boolean :sent
 
