1efab149888a16bddeb4e80c880c95e03c0a22ed
Hans Lemuet <hans@etamin.studio>
2025-10-04 21:55:19 +0200

    Messages counter cache on List

--- test/models/missive/list_test.rb
+++ test/models/missive/list_test.rb
@@ -13,5 +13,14 @@ module Missive
       assert_equal 2, list.messages.count
       assert list.messages.first.is_a?(Missive::Message)
     end
+
+    test "has a messages counter cache" do
+      list = missive_lists(:newsletter)
+      assert_equal 2, list.messages_count
+      list.messages.create!(subject: "Hello, world!")
+      assert_equal 3, list.messages_count
+      list.messages.last.destroy!
+      assert_equal 2, list.messages_count
+    end
   end
 end
