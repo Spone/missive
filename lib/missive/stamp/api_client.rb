322f88128f9ca3205261da684a10abb298854ade
Hans Lemuet <hans@etamin.studio>
2025-10-27 23:45:09 +0100

    WIP Stamp::ApiClient

--- lib/missive/stamp/api_client.rb
+++ lib/missive/stamp/api_client.rb
@@ -6,22 +6,8 @@ module Missive
   # - implementation of the [Bulk API](https://postmarkapp.com/developer/api/bulk-email)
   module Stamp
     class ApiClient < ::Postmark::ApiClient
-      # client.deliver_in_bulk(
-      #   from: "david@example.com",
-      #   subject: "Hey, you!",
-      #   html_body: "<p>Hello, {{name}}!</p>",
-      #   text_body: "Hello, {{name}}!",
-      #   messages: [
-      #     {
-      #       to: "jane.doe@example.com",
-      #       template_model: { name: "Jane" }
-      #     },
-      #     {
-      #       to: "john.doe@example.com",
-      #       template_model: { name: "John" }
-      #     }
-      #   ]
-      # )
+      # Send bulk emails
+      # https://postmarkapp.com/developer/api/bulk-email#send-bulk-emails
       def deliver_in_bulk(message, recipients = [])
         data = serialize(message.to_postmark_hash.merge(messages: recipients))
 
@@ -30,94 +16,11 @@ module Missive
         end
       end
 
+      # Get the status/details of a bulk API request
+      # https://postmarkapp.com/developer/api/bulk-email#get-a-bulk-send-status
       def get_bulk_status(id)
         format_response http_client.get("email/bulk/#{id}")
       end
     end
   end
 end
-
-# Example body
-# {
-#   "From": "david@example.com",
-#   "ReplyTo": "staff@example.com",
-#   "Subject": "This is a bulk email for {{FirstName}}",
-#   "HtmlBody": "<html><body>Hi, {{FirstName}}</body></html>",
-#   "TextBody": "Hi, {{FirstName}}",
-#   "TemplateId": null,
-#   "TemplateAlias": null,
-#   "Metadata": {
-#     "color": "blue",
-#     "client-id": "12345"
-#   },
-#   "MessageStream": "broadcast",
-#   "TrackOpens": true,
-#   "TrackLinks": "None",
-#   "Attachments": [
-#     {
-#       "Name": "readme.txt",
-#       "Content": "dGVzdCBjb250ZW50",
-#       "ContentType": "text/plain"
-#     }
-#   ],
-#   "Headers": [
-#     {
-#       "Name": "CUSTOM-HEADER",
-#       "Value": "value"
-#     }
-#   ],
-#   "InlineCss": true,
-#   "Tag": "Newsletter",
-#   "Messages": [
-#     {
-#       "To": "receiver1@example.com",
-#       "TemplateModel": {
-#         "FirstName": "Bob"
-#       },
-#       "Metadata": {
-#         "color": "blue",
-#         "client-id": "12345"
-#       },
-#       "Headers": [
-#         {
-#           "Name": "CUSTOM-HEADER",
-#           "Value": "value"
-#         }
-#       ]
-#     },
-#     {
-#       "To": "receiver2@example.com",
-#       "Cc": "cc@example.com",
-#       "TemplateModel": {
-#         "FirstName": "Frieda"
-#       },
-#       "Metadata": {
-#         "color": "blue",
-#         "client-id": "12345"
-#       },
-#       "Headers": [
-#         {
-#           "Name": "CUSTOM-HEADER",
-#           "Value": "value"
-#         }
-#       ]
-#     },
-#     {
-#       "To": "receiver3@example.com",
-#       "Bcc": "bcc@example.com",
-#       "TemplateModel": {
-#         "FirstName": "Elijah"
-#       },
-#       "Metadata": {
-#         "color": "blue",
-#         "client-id": "12345"
-#       },
-#       "Headers": [
-#         {
-#           "Name": "CUSTOM-HEADER",
-#           "Value": "value"
-#         }
-#       ]
-#     }
-#   ]
-# }
